ce2b99de4e4377aeabc8fbb3f457f2476b49bf27
Erick-Nascimento <55209050+Erick-Nascimento@users.noreply.github.com>
2025-08-27 18:33:11 -0300

    feat: send email to student and branch

--- apps/site.ts
+++ apps/site.ts
@@ -1,23 +1,32 @@
 import website, { Props } from "apps/website/mod.ts";
 import manifest, { Manifest } from "../manifest.gen.ts";
 import { type App, type AppContext as AC } from "@deco/deco";
+import { Sendgrid } from "../loaders/sendgridConfig.ts";
 type WebsiteApp = ReturnType<typeof website>;
+export interface SiteProps extends Props {
+  sendgrid: Sendgrid;
+}
 /**
  * @title Site
  * @description Start your site from a template or from scratch.
  * @category Tool
  * @logo https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1/0ac02239-61e6-4289-8a36-e78c0975bcc8
  */
-export default function Site(state: Props): App<Manifest, Props, [
-    WebsiteApp
+export default function Site(
+  { sendgrid, ...state }: SiteProps,
+): App<Manifest, SiteProps, [
+  WebsiteApp,
 ]> {
-    return {
-        state,
-        manifest,
-        dependencies: [
-            website(state),
-        ],
-    };
+  return {
+    state: {
+      sendgrid,
+      ...state,
+    },
+    manifest,
+    dependencies: [
+      website(state),
+    ],
+  };
 }
 export type SiteApp = ReturnType<typeof Site>;
 export type AppContext = AC<SiteApp>;
