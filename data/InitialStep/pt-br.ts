1b99e23e021b8b1ac3ac6a3d32a623ae22dcb066
decobot <capy@deco.cx>
2025-09-03 10:08:01 -0300

    test deploy

--- data/InitialStep/pt-br.ts
+++ data/InitialStep/pt-br.ts
@@ -1,6 +1,6 @@
 export const initialStepDataPT = {
   "introductoryText":
-    "Bem vindo ao formulário de matrícula do <strong>BRASAS</strong>.<br />Qual o tipo de curso que tem interesse em se matricular?",
+    "Bem vindo ao formulário de matrícula do <strong>BRASAS</strong>.<br />Qual o tipo de curso que tem interesse em se matricular?<br /> Olá mundo! O deploy foi feito!",
   "regularCourseButtonText": "Curso Regular",
   "pffCourseButtonText": "Portuguese for Foreigners",
 };
